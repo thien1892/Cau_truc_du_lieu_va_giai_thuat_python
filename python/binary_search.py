a1db3db0d253cc4e4748b65d36ba0f515b1c938c
thien1892 <thien1892@gmail.com>
2022-12-12 20:38:17 +0700

    update binary search

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,6 +1,21 @@
 def binary_search(arr, k):
-    min, max , mid = 0, len(arr) - 1, 0
-    while min <= max :
+    """Binary search
+
+    Parameters
+    ----------
+    arr : list
+        ordered list
+    k : float
+        value to find in list
+
+    Returns
+    -------
+    index: int
+        the index in list is value k, if k not in list return -1
+
+    """
+    min, max, mid = 0, len(arr) - 1, 0
+    while min <= max:
         mid = (min + max) // 2
         if arr[mid] > k:
             max = mid - 1
